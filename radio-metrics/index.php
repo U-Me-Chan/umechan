11545dc5ace1a526652deebae9d90570a572564a
ridouchire <ridouchire@gmail.com>
2023-02-25 04:31:26 +0400

    Улучшение посчёта статистики воспроизведений в сервисе радио-метрик

--- radio-metrics/index.php
+++ radio-metrics/index.php
@@ -11,6 +11,11 @@ use Ridouchire\RadioMetrics\Storage\Track\Track;
 use Ridouchire\RadioMetrics\Storage\Playlist\Playlist;
 use Ridouchire\RadioMetrics\Storage\Record\Record;
 
+define('ESTIMATE_VALUE_LISTENERS_NOT_CHANGE', 1);
+define('ESTIMATE_VALUE_LISTENERS_INCREASE', 100);
+define('ESTIMATE_VALUE_LISTENERS_DECREASE', 100);
+define('PLAYING_PERIOD', 10); // 10 минут
+
 $db = new Medoo([
     'database_type' => 'mysql',
     'database_name' => $_ENV['MYSQL_DATABASE'],
@@ -23,6 +28,8 @@ $db = new Medoo([
 
 $collector = new DataCollector($_ENV['RADIO_API_URL']);
 $repository = new Repository($db);
+$current_data = $collector->getData();;
+$current_data['last_start_playlist'] = time();
 
 Loop::addPeriodicTimer(1, function () use ($repository, $collector, &$current_data) {
     $data = $collector->getData();
@@ -71,12 +78,33 @@ Loop::addPeriodicTimer(1, function () use ($repository, $collector, &$current_da
     ) {
         $playlist->play_count = $artist->play_count + 1;
         $playlist->last_playing = time();
+        $current_data['last_start_playlist'] = time();
     }
 
     if ($data['listeners'] !== 0) {
-        $artist->estimate   = $artist->estimate + 1;
-        $track->estimate    = $track->estimate + 1;
-        $playlist->estimate = $playlist->estimate + 1;
+        $artist->estimate   = $artist->estimate + ESTIMATE_VALUE_LISTENERS_NOT_CHANGE;
+        $track->estimate    = $track->estimate + ESTIMATE_VALUE_LISTENERS_NOT_CHANGE;
+    }
+
+    if ($data['listeners'] > $current_data['listeners']) {
+        $artist->estimate   = $artist->estimate + ESTIMATE_VALUE_LISTENERS_INCREASE;
+        $track->estimate    = $track->estimate + ESTIMATE_VALUE_LISTENERS_INCREASE;
+        $playlist->estimate = $playlist->estimate + ESTIMATE_VALUE_LISTENERS_INCREASE;
+    }
+
+    if ($data['listeners'] < $current_data['listeners']) {
+        $artist->estimate   = $artist->estimate - ESTIMATE_VALUE_LISTENERS_DECREASE;
+        $track->estimate    = $track->estimate - ESTIMATE_VALUE_LISTENERS_DECREASE;
+        $playlist->estimate = $playlist->estimate - ESTIMATE_VALUE_LISTENERS_DECREASE;
+    }
+
+    // чтобы merging-задания в шедалере радио не засоряли статистику воспроизведения плейлистов
+    if (
+        time() - $current_data['last_start_playlist'] > PLAYING_PERIOD
+        && $data['listeners'] !== 0
+    ) {
+        $playlist->estimate = $playlist->estimate + ESTIMATE_VALUE_LISTENERS_NOT_CHANGE;
+        $current_data['last_start_playlist'] = time();
     }
 
     $artist_id   = $repository->save($artist);
@@ -84,5 +112,5 @@ Loop::addPeriodicTimer(1, function () use ($repository, $collector, &$current_da
     $playlist_id = $repository->save($playlist);
     $repository->save(Record::draft($artist_id, $track_id, $playlist_id, $data['listeners']));
 
-    $current_data = $data;
+    $current_data = array_merge($current_data, $data);
 });
