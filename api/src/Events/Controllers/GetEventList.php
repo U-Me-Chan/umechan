d076b0ad1a3a417c1e054cd308059c927ceb337f
kugimiya <bp.001@yandex.ru>
2024-06-20 17:18:11 +0400

    Чан: API: GetEventsList: фикс обращения к параметру

--- api/src/Events/Controllers/GetEventList.php
+++ api/src/Events/Controllers/GetEventList.php
@@ -14,7 +14,7 @@ final class GetEventList
     {
         $limit = $req->getParams('limit') ? $req->getParams('limit') : 20;
         $offset = $req->getParams('offset') ? $req->getParams('offset') : 0;
-        $from_timestamp = $vars['from_timestamp'] ? $vars['from_timestamp'] : 0;
+        $from_timestamp = $req->getParams('from_timestamp') ? $req->getParams('from_timestamp') : 0;
 
         try {
             list($posts, $count) = $this->storage->find($limit, $offset, $from_timestamp);
