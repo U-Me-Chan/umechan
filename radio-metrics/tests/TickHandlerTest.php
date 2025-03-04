9cd74c3666802efc36c002c76a02b4e0c2d9c3d4
ridouchire <ridouchire@gmail.com>
2025-03-04 11:50:50 +0400

    [#62] Радио: Метрики: Тесты исправлены

--- radio-metrics/tests/TickHandlerTest.php
+++ radio-metrics/tests/TickHandlerTest.php
@@ -7,7 +7,6 @@ use Ridouchire\RadioMetrics\Collectors\IcecastCollector;
 use Ridouchire\RadioMetrics\Collectors\MpdCollector;
 use Ridouchire\RadioMetrics\Exceptions\EntityNotFound;
 use Ridouchire\RadioMetrics\ICache;
-use Ridouchire\RadioMetrics\SenderProvider;
 use Ridouchire\RadioMetrics\Storage\Entites\Track;
 use Ridouchire\RadioMetrics\Storage\RecordRepository;
 use Ridouchire\RadioMetrics\Storage\TrackRepository;
@@ -19,7 +18,6 @@ class TickHandlerTest extends TestCase
     private Logger|MockObject $logger;
     private MpdCollector|MockObject $mpd_collector;
     private IcecastCollector|MockObject $icecast_collector;
-    private SenderProvider|MockObject $sender_provider;
     private TrackRepository|MockObject $track_repo;
     private RecordRepository|MockObject $record_repo;
     private Md5Hash|MockObject $md5hash;
@@ -37,9 +35,6 @@ class TickHandlerTest extends TestCase
         /** @var IcecastCollector|MockObject */
         $this->icecast_collector = $this->createMock(IcecastCollector::class);
 
-        /** @var SenderProvider|MockObject */
-        $this->sender_provider = $this->createMock(SenderProvider::class);
-
         /** @var TrackRepository|MockObject */
         $this->track_repo = $this->createMock(TrackRepository::class);
 
@@ -56,7 +51,6 @@ class TickHandlerTest extends TestCase
             $this->logger,
             $this->mpd_collector,
             $this->icecast_collector,
-            $this->sender_provider,
             $this->track_repo,
             $this->record_repo,
             $this->md5hash,
@@ -126,8 +120,9 @@ class TickHandlerTest extends TestCase
             'hash'          => 'hash'
         ]));
         $this->icecast_collector->expects($this->once())->method('getData')->willReturn(['listeners' => 2]);
-        $this->cache->expects($this->atLeast(3))->method('set');
+        $this->cache->expects($this->atLeast(2))->method('set');
         $this->cache->expects($this->once())->method('increment');
+        $this->record_repo->expects($this->once())->method('save');
 
         $this->handler->handle();
     }
@@ -149,8 +144,9 @@ class TickHandlerTest extends TestCase
             'hash'          => 'hash'
         ]));
         $this->icecast_collector->expects($this->atLeast(2))->method('getData')->willReturn(['listeners' => 2]);
-        $this->cache->expects($this->atLeast(5))->method('set');
+        $this->cache->expects($this->atLeast(4))->method('set');
         $this->cache->expects($this->atLeast(2))->method('increment');
+        $this->record_repo->expects($this->atLeast(2))->method('save');
 
         $this->handler->handle();
 
@@ -169,7 +165,6 @@ class TickHandlerTest extends TestCase
         ]));
 
         $this->track_repo->expects($this->once())->method('save');
-        $this->record_repo->expects($this->once())->method('save');
 
         $this->handler->handle();
     }
