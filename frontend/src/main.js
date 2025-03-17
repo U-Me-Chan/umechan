d752cf5fe51611cab36e7b743a0e51008831a299
Maxim Dejnew <overmaxcoast@yandex.ru>
2025-03-18 00:23:56 +0700

    Чан: Frontend: Исключено использование CDN для интеграции иконок mdi

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,8 @@
-import Vue from 'vue'
-import App from './components/App.vue'
-import VueRouter from 'vue-router'
-import VueCookie from 'vue-cookie'
+import Vue from 'vue';
+import App from './components/App.vue';
+import VueRouter from 'vue-router';
+import VueCookie from 'vue-cookie';
+
 import {
   Toast,
   Sidebar,
@@ -21,31 +22,33 @@ import {
   Tooltip,
   Loading,
   Taginput
-} from 'buefy'
-import 'buefy/dist/buefy.css'
+} from 'buefy';
 
-Vue.config.productionTip = true
-Vue.use(VueCookie)
-Vue.use(Toast)
-Vue.use(Sidebar)
-Vue.use(Menu)
-Vue.use(Button)
-Vue.use(Modal)
-Vue.use(Input)
-Vue.use(Field)
-Vue.use(Select)
-Vue.use(Table)
-Vue.use(Tag)
-Vue.use(Switch)
-Vue.use(Collapse)
-Vue.use(Pagination)
-Vue.use(Upload)
-Vue.use(Icon)
-Vue.use(Tooltip)
-Vue.use(Loading)
-Vue.use(Taginput)
+import 'buefy/dist/buefy.css';
+import '@mdi/font/css/materialdesignicons.min.css';
 
-Vue.use(VueRouter)
+Vue.config.productionTip = true;
+Vue.use(VueCookie);
+Vue.use(Toast);
+Vue.use(Sidebar);
+Vue.use(Menu);
+Vue.use(Button);
+Vue.use(Modal);
+Vue.use(Input);
+Vue.use(Field);
+Vue.use(Select);
+Vue.use(Table);
+Vue.use(Tag);
+Vue.use(Switch);
+Vue.use(Collapse);
+Vue.use(Pagination);
+Vue.use(Upload);
+Vue.use(Icon);
+Vue.use(Tooltip);
+Vue.use(Loading);
+Vue.use(Taginput);
+
+Vue.use(VueRouter);
 
 const routes = [
     {
@@ -76,15 +79,15 @@ const routes = [
       path: '/admin/delete-post/:id',
       component: () => import('./components/DeletePost.vue')
     }
-]
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
