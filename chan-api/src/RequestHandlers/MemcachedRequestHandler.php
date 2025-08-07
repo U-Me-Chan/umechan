813b8349c3040a10f96b23af8b0b22285db3e40d
ridouchire <ridouchire@gmail.com>
2025-08-07 14:54:30 +0400

    Чан: API: Улучшен механизм кеширования ответов

--- chan-api/src/RequestHandlers/MemcachedRequestHandler.php
+++ chan-api/src/RequestHandlers/MemcachedRequestHandler.php
@@ -8,40 +8,47 @@ use PK\RequestHandler;
 
 class MemcachedRequestHandler extends RequestHandler
 {
+    private const CACHE_KEY = 'chan-api-response-caches';
+
     private \Memcached $memcached;
 
     public function __construct(
+        private ?RequestHandler $sucessor,
         private string $host = 'memcached',
         private int $port = 11211,
-        private ?RequestHandler $sucessor = null
+        private array $cache_map = []
     ) {
         parent::__construct($sucessor);
 
         $this->memcached = new \Memcached();
         $this->memcached->addServer($this->host, $this->port);
+
+        $_cache = $this->memcached->get(self::CACHE_KEY);
+
+        $this->cache_map = $_cache === false ? [] : $_cache;
     }
 
     protected function processing(Request $req): ?Response
     {
         if ($req->getMethod() !== 'GET') {
+            $this->memcached->delete(self::CACHE_KEY); // удаляем кеш всех запросов при любой операции записи
+
             return null;
         }
 
-        if ($req->getParams('no_cache') == true) {
-            return null;
-        }
+        if (!isset($this->cache_map[$req->getHash()])) {
+            $res = $this->sucessor->handle($req);
 
-        $res = $this->memcached->get($req->getHash());
+            $this->cache_map[$req->getHash()] = $res;
 
-        if ($res !== false && $res instanceof Response) {
+            $this->memcached->set(self::CACHE_KEY, $this->cache_map);
+
             return $res;
         }
 
-        if ($res === false) {
-            $res = $this->sucessor->handle($req);
+        $res = $this->cache_map[$req->getHash()];
 
-            $this->memcached->set($req->getHash(), $res, 30);
-
+        if ($res !== false && $res instanceof Response) {
             return $res;
         }
 
