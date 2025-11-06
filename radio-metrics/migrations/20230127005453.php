17c467b1ae33550d5ac308c2b04427aa12aa97ea
ridouchire <ridouchire@gmail.com>
2025-11-06 17:23:49 +0400

    Радио: Метрики: Миграции были ревизированы

--- radio-metrics/migrations/20230127005453.php
+++ radio-metrics/migrations/20230127005453.php
@@ -20,28 +20,35 @@ final class V20230127005453 extends AbstractMigration
     {
         if (!$this->hasTable('tracks')) {
             $this->table('tracks', ['id' => false, 'primary_key' => ['id']])
-                 ->addColumn('id', 'integer', ['null' => false, 'identity' => true, 'signed' => false])
-                 ->addColumn('track', 'string')
-                 ->addIndex(['track'])
-                 ->addColumn('first_playing', 'integer')
-                 ->addColumn('last_playing', 'integer')
-                 ->addIndex(['last_playing'])
-                 ->addColumn('play_count', 'integer')
-                 ->addColumn('estimate', 'integer')
-                 ->addIndex(['estimate'])
+                ->addColumn('id', 'integer', ['null' => false, 'identity' => true, 'signed' => false])
+                ->addColumn('track', 'string')
+                ->addIndex(['track'])
+                ->addColumn('first_playing', 'integer')
+                ->addColumn('last_playing', 'integer')
+                ->addIndex(['last_playing'])
+                ->addColumn('play_count', 'integer')
+                ->addColumn('estimate', 'integer')
+                ->addIndex(['estimate'])
+                ->addColumn('duration', 'integer', ['signed' => false, 'null' => true])
+                ->addColumn('path', 'string', ['limit' => 255, 'null' => true])
+                ->addColumn('mpd_track_id', 'integer', ['signed' => false, 'null' => true])
+                ->addColumn('artist', 'string', ['limit' => 255, 'null' => true])
+                ->addColumn('title', 'string', ['limit' => 255, 'null' => true])
+                ->addColumn('hash', 'string', ['limit' => 255])
+                ->addIndex(['hash'])
                 ->create();
         }
 
         if (!$this->hasTable('records')) {
             $this->table('records')
-                 ->addColumn('track_id', 'integer', ['null' => false, 'signed' => false])
-                 ->addForeignKey('track_id', 'tracks', 'id')
-                 ->addIndex(['track_id'])
-                 ->addColumn('listeners', 'integer')
-                 ->addIndex(['listeners'])
-                 ->addColumn('timestamp', 'integer')
-                 ->addIndex(['timestamp'])
-                 ->create();
+                ->addColumn('track_id', 'integer', ['null' => false, 'signed' => false])
+                ->addForeignKey('track_id', 'tracks', 'id')
+                ->addIndex(['track_id'])
+                ->addColumn('listeners', 'integer')
+                ->addIndex(['listeners'])
+                ->addColumn('timestamp', 'integer')
+                ->addIndex(['timestamp'])
+                ->create();
         }
     }
 }
