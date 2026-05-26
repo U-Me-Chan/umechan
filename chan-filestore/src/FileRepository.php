ccf30ca6036e8d7a2dd431ba6478d4890a136494
ridouchire <ridouchire@gmail.com>
2026-05-26 05:42:29 +0400

    Чан: Хранилище Файлов: Улучшена производительность выполнения запроса списка файлов

--- chan-filestore/src/FileRepository.php
+++ chan-filestore/src/FileRepository.php
@@ -2,10 +2,10 @@
 
 namespace IH;
 
-use IH\Utils\DirectoryIterator;
 use InvalidArgumentException;
-use Medoo\Medoo;
 use SplFileInfo;
+use Medoo\Medoo;
+use IH\Utils\DirectoryIterator;
 
 class FileRepository
 {
@@ -28,9 +28,41 @@ class FileRepository
         $files = $this->directory_iterator->getSlice($offset, $limit);
         $count = $this->directory_iterator->count();
 
-        $files = array_map(function (SplFileInfo $spl_file) {
-            $file_name = $spl_file->getBasename();
+        if (sizeof($files) == 0) {
+            return new FileCollection([], $count, $filters);
+        }
 
+        /** @var string[] */
+        $files_names = array_map(fn(SplFileInfo $file) => $file->getBasename(), $files);
+
+        // TODO: однажды весь этот CTE следует заменить хранением ссылок на посты в хранилище
+        $cte_first_query = sprintf("WITH search_names AS( SELECT %s AS name ", $this->db->quote(array_shift($files_names)));
+
+        if (sizeof($files_names) !== 1) {
+            foreach ($files_names as $name) {
+                $cte_first_query .= sprintf("UNION ALL SELECT %s ", $this->db->quote($name));
+            }
+        }
+
+        $query = $cte_first_query . "), matched_posts AS (
+        SELECT
+            search_names.name AS file_name,
+            posts.id AS post_id
+        FROM search_names
+        LEFT JOIN posts ON posts.message LIKE CONCAT('%', search_names.name, '%')
+        )
+        SELECT
+            file_name,
+            GROUP_CONCAT(post_id ORDER BY post_id SEPARATOR ',') AS post_ids
+        FROM matched_posts
+        GROUP BY file_name;";
+
+        $result = $this->db->query($query)->fetchAll($this->db->pdo::FETCH_ASSOC);
+
+        $files = array_map(function (array $file_data) {
+            $file_name = $file_data['file_name'];
+
+            // TODO: однажды всё это должно переехать в какое-то хранилище
             if (
                 substr($file_name, -3) == 'mp4' ||
                 substr($file_name, -4) == 'webm' ||
@@ -44,22 +76,15 @@ class FileRepository
 
             $original = $this->static_url . '/' . $file_name;
 
-            $post_ids = $this->db->select('posts', 'id', [ // @phpstan-ignore arguments.count,argument.type,argument.type
-                'message[~]' => '%' . $file_name . '%'
-            ]);
-
-            if (!$post_ids) { // @phpstan-ignore booleanNot.alwaysTrue
-                $post_ids = [];
-            }
+            $post_ids = $file_data['post_ids'] === null ? [] : explode(',', $file_data['post_ids']);
 
             return new File(
                 $file_name,
                 $original,
                 $thumbnail,
                 $post_ids
-
             );
-        }, $files);
+        }, $result);
 
         return new FileCollection($files, $count, $filters);
     }
